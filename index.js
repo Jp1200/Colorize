dc26f73eb3b9a004ffffeb6dcd5a2f86a8db1084
Jp1200 <jprkr96@gmail.com>
2020-11-19 10:15:33 -0600

    js file init

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 window.addEventListener('DOMContentLoaded', (event) => {
     console.log('DOM fully loaded and parsed');
-
-
+    const about = document.getElementById('about');
+    const home = document.getElementById('home');
+    const contact = document.getElementById('contact');
+    const file = document.getElementById('file');
     const btn = document.getElementById('btn');
-    btn.addEventListener('click', ()=>{
-        console.log("clicked")
-        alert("Photo sent to server")
+    const mainDiv = document.getElementsByClassName("main-div")
+    btn.addEventListener('click', (event)=>{
+
+        event.preventDefault();
+        if (file){
+            console.log(file);
+        }
+        else{
+            alert("No file selected");
+        }
+    });
+    about.addEventListener('clicked', ()=>{
+        
+        document.getElementById('AboutMeHTML').style = ""
+    });
+    home.addEventListener('click', ()=>{
+
+    });
+    contact.addEventListener('clicked', ()=>{
+
     });
 });
